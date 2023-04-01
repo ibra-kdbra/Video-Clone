acf5e31acba6886b6b4dcb1a96a3c38462c3bd21
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-01 19:08:15 +0300

    editing some code for the deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,7 @@ import SchoolIcon from '@mui/icons-material/School';
 import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
 import CheckroomIcon from '@mui/icons-material/Checkroom';
 import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import BiotechOutlinedIcon from '@mui/icons-material/BiotechOutlined';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
@@ -16,7 +17,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  // { name: 'JS Mastery', icon: <CodeIcon />, },
+  { name: 'Andrew Huberman', icon: <BiotechOutlinedIcon/>, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
@@ -34,9 +35,9 @@ export const categories = [
   { name: 'Crypto', icon: <DeveloperModeIcon />, },
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoThumbnailUrl = 'https://i3.ytimg.com/vi/QmOF0crdyRU/maxresdefault.jpg';
+export const demoChannelUrl = '/channel/@hubermanlab';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoChannelTitle = 'Andrew Huberman';
+export const demoVideoTitle = 'Controlling Your Dopamine For Motivation, Focus & Satisfaction | Huberman Lab Podcast #39';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
